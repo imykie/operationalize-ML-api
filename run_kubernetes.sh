db961eb06b32b4c95876cd9ad9a182dbd6ba9a7e
Michael Olatunji <tundexmike@gmail.com>
2020-05-27 14:56:43 +0100

    Adding run_kubenetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,5 +1,13 @@
 #!usr/bin/env bash
 
-#docker path
+#docker path, image from docker hub
 dockerpath="imykel/operationalized-ml-api"
 
+#kubectl run ml-api --image=$dockerpath --port=80
+kubectl run flaskapp --generator=run-pod/v1 --image=$dockerpath --port=80 --labels ml-api=flaskapp
+
+# List k8s pods
+kubectl get pods
+
+# Forward the container port to host 
+kubectl port-forward flaskapp 8000:80
